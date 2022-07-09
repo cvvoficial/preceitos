3ad099aab6f77294018dcc65270faa21ceee0eae
José Fernando Tolentino <josefernandotolentino@gmail.com>
2022-07-09 15:07:17 +0100

    nova logo

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,26 @@ import { useDeckState } from './state/dockState'
 
 export function App() {
   const deckState = useDeckState();
-  return (
+  const [deckVisible, setDeckVisible] = useState(false)
+  return deckVisible ?
     <div className={`app`}>
       <div className={`flex fill center container`}>
         <Deck deckState={deckState} />
       </div>
       <div className={`buttons`}>
-        <button className="button" role="button" onClick={() => deckState.reset()}>Recomeçar</button>
-        <button className="button" role="button" onClick={() => deckState.shuffle()}>Embaralhar</button>
+        <button className="button" role="button" onClick={reset}>Recomeçar</button>
+        <button className="button" role="button" onClick={shuffle}>Sortear</button>
       </div>
+    </div> : <div className={`app fill center container buttonsA`}>
+        <button className="button" role="button" onClick={reset}>Ver preceitos</button>
+        <button className="button" role="button" onClick={shuffle}>Sortear preceito</button>
     </div>
-  )
+    function reset() {
+      setDeckVisible(true);
+      deckState.reset()  
+    }
+    function shuffle() {
+      setDeckVisible(true);
+      deckState.shuffle()  
+    }
 }
