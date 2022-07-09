68879d062ffbfd8ca4edd32cdc4209aff9312434
José Fernando Tolentino <josefernandotolentino@gmail.com>
2022-07-09 18:12:15 +0100

    gift

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,6 +4,7 @@ export function Home({ deckState }: { deckState: DeckState }) {
     return <div className={`app fill center container buttonsH`}>
         <button className="button" role="button" onClick={reset}>Ver preceitos</button>
         <button className="button" role="button" onClick={shuffle}>Sortear preceito</button>
+        <button className="button" role="button" onClick={makeGift}>Presentear alguém</button>
     </div>;
     function reset() {
         deckState.setPage('work')
@@ -12,6 +13,9 @@ export function Home({ deckState }: { deckState: DeckState }) {
     function shuffle() {
         deckState.setPage('work')
         deckState.shuffle()
+    }
+    function makeGift() {
+        deckState.setPage('make-gift')
     }
 }
 
