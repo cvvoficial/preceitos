b6a12d44d858dcfe6a2f586200ebbad09ebb5b47
José Fernando Tolentino <josefernandotolentino@gmail.com>
2022-08-24 08:39:58 +0100

    fix: preceito 23 / gift

--- src/pages/gitf.tsx
+++ src/pages/gitf.tsx
@@ -1,69 +1,31 @@
 import { DeckState } from '../state/dockState'
-import gift from '../gift.png';
 import './gift.css'
 import { CSSProperties, useLayoutEffect, useRef, useState } from 'react';
+import { GiftImage } from '../images/gift'
 
 export function Gift() {
     const giftRef = useRef<HTMLImageElement>(null)
-    const [cnt, setCnt] = useState(1)
-    const [deStyle, setDeStyle] = useState<CSSProperties>({ visibility: 'collapse' })
-    const [paraStyle, setParaStyle] = useState<CSSProperties>({ visibility: 'collapse' })
     const [de, para] = extractFields()
-    useLayoutEffect(() => {
-        const img = giftRef.current
-        if (img) {
-            setTimeout(render, 10)
-            window.addEventListener('resize', refresh)
-            return () => {
-                window.removeEventListener('resize', refresh)
-            }
-        }
-        function refresh() {
-            setCnt((c) => (c + 1) % 1000)
-        }
-    }, [giftRef.current, cnt])
-    return <div className={`app fill center container`} style={{ position: 'relative' }} onClick={openGift}>
-        <img src={gift} className="gift" ref={giftRef} />
-        <div style={deStyle}>{de}</div>
-        <div style={paraStyle}>{para}</div>
+    return <div className={`app fill center container gift-container`} onClick={openGift}>
+        <GiftImage className="gift-box" />
+        <div className="gift-texts">
+            <div className="gift-para">
+                Para:<br />
+                <span className="gift-text">
+                    {para}
+                </span>
+            </div>
+            <div className="gift-de">
+                De:<br />
+                <span className="gift-text">
+                    {de}
+                </span>
+            </div>
+        </div>
     </div>;
     function openGift() {
         window.location.hash = ''
-        setTimeout(() => window.location.reload(),10)
-    }
-    function render() {
-        const img = giftRef.current
-        if (!img) return
-        const bounds = img.getBoundingClientRect()
-        const left = bounds.left;
-        const top = bounds.top;
-        const height = bounds.bottom - top;
-        const width = bounds.right - left;
-        if (!(height && width)) {
-            console.log({ height, width, cnt })
-            setCnt(c => c + 1)
-            return
-        }
-        setDeStyle({
-            position: 'absolute',
-            visibility: 'visible',
-            zIndex: 1,
-            left: Math.round(left + (width - left) * 0.20),
-            top: Math.round(top + (height - top) * 0.49),
-            maxWidth: Math.round(width * 0.25),
-            transform: 'rotate(13deg)',
-            fontSize: 38,
-        })
-        setParaStyle({
-            position: 'absolute',
-            visibility: 'visible',
-            zIndex: 1,
-            left: Math.round(left + (width - left) * 0.24),
-            top: Math.round(top + (height - top) * 0.63),
-            maxWidth: Math.round(width * 0.25),
-            transform: 'rotate(13deg)',
-            fontSize: 38,
-        })
+        setTimeout(() => window.location.reload(), 10)
     }
     function extractFields(): [string, string] {
         const hash = window.location.hash
